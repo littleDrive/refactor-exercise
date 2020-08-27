076054f882bbd8629a5a6281d0373a4881447995
littleDrive <ZERO.OU@OOCL.COM>
2020-08-27 15:18:51 +0800

    [refactor-exercise][zero]refactor: remove unnecessary comments

--- src/statement.js
+++ src/statement.js
@@ -28,11 +28,8 @@ function statement (invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-    // add volume credits
     volumeCredits += Math.max(performance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(performance.audience / 5);
-    //print line for this order
     result += ` ${play.name}: ${format(thisAmount / 100)} (${performance.audience} seats)\n`;
     totalAmount += thisAmount;
   }
