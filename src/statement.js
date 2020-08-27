66a415a697e3b3112308bdf70af45c60ac46f8c2
littleDrive <ZERO.OU@OOCL.COM>
2020-08-27 15:30:57 +0800

    [refactor-exercise][zero]refactor: add currencyFormat

--- src/statement.js
+++ src/statement.js
@@ -1,12 +1,16 @@
+const currencyFormat = () => {
+    return new Intl.NumberFormat('en-US', {
+               style: 'currency',
+               currency: 'USD',
+               minimumFractionDigits: 2,
+             }).format;
+}
+
 function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
+  const format = currencyFormat();
 
   for (let performance of invoice.performances) {
     const play = plays[performance.playID];
